ccf1c425c26f172838665a6a6dcd0fe8ee3e2264
COLORFUL-ADJIE\Adjie <adjie9876@gmail.com>
2026-02-14 11:33:03 +0700

    Setup framerate of sprite, load enemy

--- dasher.cpp
+++ dasher.cpp
@@ -10,13 +10,17 @@ int main(){
     //Acceleration due to gravity (pixels/sec)/second
     const int gravity{1'000};
 
-    //texture sprite
+    //texture & variable of scarfy (character)
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
     scarfyRec.width = scarfy.width/6; //because the sprite has 6 image
     scarfyRec.height = scarfy.height;
     scarfyRec.x = 0;
     scarfyRec.y = 0;
+
+    //texture & variable of nebula (enemy)
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+
 
     //exactly lined in the center
     Vector2 scarfyPos;
@@ -25,6 +29,13 @@ int main(){
     
     //Initial position
     int velocity{0};
+
+    //animation frame
+    int frame{};
+
+    //amount of time before we update the animation frame
+    const float updateTime{1.0/12.0};
+    float runningTime{};
 
     //check rectangle is in the air or not
     bool isInAir{true};
@@ -41,7 +52,12 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
-        //Perform ground check
+        //Movement command
+        if(IsKeyPressed(KEY_SPACE) && !isInAir){//checking so no double jumping
+            velocity += jumpVelocity;
+        }
+
+        //Set movement, perform ground check
         if(scarfyPos.y >= window_height - scarfyRec.width){
             //rectangle is in the ground
             velocity = 0;
@@ -52,14 +68,21 @@ int main(){
             isInAir = true;
         }
 
-        //Movement command
-        if(IsKeyPressed(KEY_SPACE) && !isInAir){//checking so no double jumping
-            velocity += jumpVelocity;
-        }
-
         //Update position
         scarfyPos.y += velocity * dT;//posY = posY + velocity * dT
-        
+
+        //Update running time
+        runningTime += dT;
+        if(runningTime >= updateTime){
+            runningTime = 0.0;
+            //Update animation frame
+            scarfyRec.x = frame * scarfyRec.width;
+            frame++;
+            if(frame > 5){
+                frame = 0;
+            }
+        }
+
         //Draw the object
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
@@ -67,8 +90,10 @@ int main(){
         EndDrawing();
     }
     
-    UnloadTexture(scarfy); //Unload it from leaking the GPU VRAM
-    CloseWindow(); //Close window so it doesn't leak
+    //Unload it from leaking the GPU VRAM
+    UnloadTexture(scarfy);
+    UnloadTexture(nebula);
+    CloseWindow();
 
     return 0;
 }
